26eb6b293430dbc29f65b0686c5c5323015d052e
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-10-20 18:34:25 +0200

    spotify-3 No longer logged in when auth token is expired

--- src/helper/helperfunctions.js
+++ src/helper/helperfunctions.js
@@ -1,6 +1,17 @@
-const getAuthToken = () => localStorage.getItem('auth_token')
+import moment from 'moment'
 
-const setAuthToken = token => localStorage.setItem('auth_token', token)
+
+const getAuthToken = () => {
+    const token_updated = localStorage.getItem('token_updated')
+    if (token_updated && moment().subtract(60, 'minutes').isBefore(moment(token_updated))) {
+        return localStorage.getItem('auth_token')
+    }
+}
+
+const setAuthToken = token => {
+    localStorage.setItem('auth_token', token)
+    localStorage.setItem('token_updated', moment().toISOString())
+}
 
 const getQueryString = (url, params) => {
     if (Object.keys(params).length === 0) {
