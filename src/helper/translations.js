c4daaa4595aa422d01ad6e2a9c923a669d0e9849
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-12-08 13:58:17 +0100

    Added translations

--- src/helper/translations.js
+++ src/helper/translations.js
@@ -4,8 +4,17 @@ const translations = {
         LOGIN_WITH_SPOTIFY: 'Login with Spotify',
         SONG_TITLE: 'Title',
         ARTISTS: 'Artists',
+        ARTIST: 'Artist',
         RELEASE_DATE: 'Release date',
-        LISTEN: 'Listen'
+        LISTEN: 'Listen',
+        ALBUM: 'Album',
+        PREVIEW: 'Preview',
+        DURATION: 'Duration',
+        FAVORITE_ARTISTS: 'Favorite Artists',
+        FAVORITE_SONGS: 'Favorite Songs',
+        RANK: 'Rank',
+        FOLLOWERS: 'Followers',
+        POPULARITY: 'Popularity'
     },
     de: {
         SEARCH_SONGS_BY_ARTIST: 'Suche Songs nach Künstler',
@@ -13,7 +22,15 @@ const translations = {
         SONG_TITLE: 'Titel',
         ARTISTS: 'Künstler',
         RELEASE_DATE: 'Erscheinungsdatum',
-        LISTEN: 'Hören'
+        LISTEN: 'Hören',
+        ALBUM: 'Album',
+        PREVIEW: 'Vorschau',
+        DURATION: 'Länge',
+        FAVORITE_ARTISTS: 'Lieblingskünstler',
+        FAVORITE_SONGS: 'Lieblingssong',
+        RANK: 'Rang',
+        FOLLOWERS: 'Follower',
+        POPULARITY: 'Beliebtheit'
     }
 }
 
